5d1e948c15f9a81c80c078a44e1805fe5174ef6b
ARONEZG <sverk97@gmail.com>
2025-08-11 09:00:43 +0300

    added 5 rule

--- array.h
+++ array.h
@@ -8,31 +8,89 @@ class DynamicArray final {
 public:
     DynamicArray() = default;
 
-    ~DynamicArray() {}
+    ~DynamicArray() {
+        if (_ptr != nullptr) {
+            for (size_t i = 0; i < _size; ++i) {
+                _ptr[i].~T();
+            }
+            ::operator delete(_ptr);
+        }
+        _ptr = nullptr;
+        _size = 0;
+        _capacity = 0;
+    }
 
     DynamicArray(size_t size) {
         _capacity = size * 2;
         _ptr = static_cast<T *>(::operator new(_capacity * sizeof(T)));
-        new (_ptr) T[size];
-        _size = size;
+        int constructed = 0;
+        try {
+            for (; constructed < size; constructed++) {
+                new (_ptr + constructed) T();
+            }
+
+            _size = size;
+        } catch (...) {
+            destruct(constructed);
+            throw;
+        }
+    }
+
+    DynamicArray(const T *begin, int size) {
+        _capacity = size * 2;
+        _ptr = static_cast<T *>(::operator new(_capacity * sizeof(T)));
+        int constructed = 0;
+
+        try {
+            for (; constructed < size; ++constructed) {
+                new (_ptr + constructed) T(begin[constructed]);
+            }
+
+            _size = size;
+        } catch (...) {
+            destruct(constructed);
+            throw;
+        }
     }
 
     DynamicArray(const DynamicArray &ohter) {
         _size = ohter._size;
         _capacity = ohter._capacity;
         _ptr = static_cast<T *>(::operator new(_capacity * sizeof(T)));
-        for (size_t i = 0; i < _size; ++i) {
-            new (_ptr + i) T(ohter._ptr[i]);
+        int constructed = 0;
+        try {
+            for (; constructed < _size; ++constructed) {
+                new (_ptr + constructed) T(ohter._ptr[constructed]);
+            }
+        } catch (...) {
+            destruct(constructed);
+            throw;
         }
     }
 
-    DynamicArray(DynamicArray &&ohter) {
-        _size = ohter._size;
-        _capacity = ohter._capacity;
-        _ptr = ohter._ptr;
-        ohter._ptr = nullptr;
-        ohter._size = 0;
-        ohter._capacity = 0;
+    DynamicArray &operator=(DynamicArray other) {
+        swap(other);
+        return *this;
+    }
+
+    DynamicArray &operator=(DynamicArray &&other) {
+        swap(other);
+        return *this;
+    }
+
+    DynamicArray(DynamicArray &&other) {
+        _size = other._size;
+        _capacity = other._capacity;
+        _ptr = other._ptr;
+        other._ptr = nullptr;
+        other._size = 0;
+        other._capacity = 0;
+    }
+
+    void swap(DynamicArray &other) {
+        std::swap(_ptr, other._ptr);
+        std::swap(_size, other._size);
+        std::swap(_capacity, other._capacity);
     }
 
     void resize(size_t size) {
@@ -43,44 +101,57 @@ public:
 
         auto resized = static_cast<T *>(::operator new(size * 2 * sizeof(T)));
 
-        for (size_t i = 0; i < _size; ++i) {
-            new (resized + i) T(std::move(*(_ptr + i)));
+        int constructed = 0;
+
+        try {
+            for (; constructed < _size; ++constructed) {
+                new (resized + constructed) T(std::move(*(_ptr + constructed)));
+            }
+
+            for (; constructed < size; ++constructed) {
+                new (resized + constructed) T();
+            }
+
+            destruct(constructed);
+            _capacity = size * 2;
+            _size = size;
+            _ptr = resized;
+
+        } catch (...) {
+            destruct(constructed);
+            throw;
         }
-
-        for (size_t i = _size; i < size; ++i) {
-            new (resized + i) T();
-        }
-
-        _capacity = size * 2;
-        _size = size;
-
-        delete[] _ptr;
-        _ptr = resized;
     }
 
     void pushBack(T &&item) {
-        if (_capacity < _size + 1 && _size != 0 && _ptr != nullptr) {
+        if (_size < _capacity) {
             new (_ptr + _size) T(std::forward<T>(item));
-            _size = _size + 1;
+            _size++;
             return;
         }
 
         auto resized =
             static_cast<T *>(::operator new((_size + 1) * 2 * sizeof(T)));
 
-        for (size_t i = 0; i < _size; ++i) {
-            new (resized + i) T(std::move(*(_ptr + i)));
+        int constructed = 0;
+        try {
+            for (; constructed < _size; ++constructed) {
+                new (resized + constructed) T(std::move(*(_ptr + constructed)));
+            }
+
+            new (resized + _size) T(std::forward<T>(item));
+            _size++;
+            _capacity = _size * 2;
+
+            ::operator delete(_ptr);
+            _ptr = resized;
+        } catch (...) {
+            destruct(constructed);
+            throw;
         }
-
-        new (resized + _size) T(std::forward<T>(item));
-        _size++;
-        _capacity = _size * 2;
-
-        delete[] _ptr;
-        _ptr = resized;
     }
 
-    T operator[](int index) const {
+    T &operator[](int index) const {
         if (index >= _size) {
             throw std::runtime_error("index >= size");
         }
@@ -100,4 +171,15 @@ private:
     T *_ptr = nullptr;
     size_t _size = 0;
     size_t _capacity = 0;
+
+    void destruct(int constructed) {
+        for (int i = 0; i < constructed; ++i) {
+            _ptr[i].~T();
+        }
+
+        ::operator delete(_ptr);
+        _ptr = nullptr;
+        _size = 0;
+        _capacity = 0;
+    }
 };
